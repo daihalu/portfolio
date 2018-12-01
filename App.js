8bf78a4744684c4cc54ab328820680c0ffaf6fb0
daihalu <nvdai.halutech@gmail.com>
2018-12-01 17:33:45 +0700

    Deploy

--- App.js
+++ App.js
@@ -7,9 +7,9 @@ app.use(express.static(path.join(__dirname, '/dist')))
 app.use(bodyParser.json())
 
 app.use(bodyParser.urlencoded({extended: true}))
-// app.get('/', (req,res) => {
-//   res.sendFile('./dist/index.html');
-// });
+app.get('/', (req,res) => {
+  res.sendFile('./dist/index.html');
+});
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
