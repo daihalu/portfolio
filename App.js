b7fc5b2328bc18068fe60e44fc2699f818886117
daihalu <nvdai.halutech@gmail.com>
2018-12-03 07:37:20 +0700

    Deploy

--- App.js
+++ App.js
@@ -2,14 +2,13 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const app = express()
-const port = process.env.port || 8080
-app.use(express.static(path.join(__dirname, '/dist')))
+const port = process.env.PORT || 8080
+app.use('/', express.static(path.join(__dirname, '/dist')))
 app.use(bodyParser.json())
 
 app.use(bodyParser.urlencoded({extended: true}))
-app.get('/', (req,res) => {
-  res.sendFile('./dist/index.html');
-});
-
+// app.get('/', (req,res) => {
+//   res.sendFile('./dist/index.html');
+// });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
